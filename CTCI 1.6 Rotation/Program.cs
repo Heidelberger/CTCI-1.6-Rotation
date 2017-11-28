314ad66bcbd36e82fd449dd864b7480d02bad3cb
Qubert <upafourth@gmail.com>
2017-11-28 21:39:20 +0100

    Refactored code, made performance enhancements

--- CTCI 1.6 Rotation/Program.cs
+++ CTCI 1.6 Rotation/Program.cs
@@ -13,21 +13,87 @@ namespace CTCI_1._6_Rotation
         {
             PrintHeaderMsg(1, 6, "Rotation");
 
-            int[,] array2D = new int[4, 4] {{00,01,02,03},{10,11,12,13},{20,21,22,23},{30,31,32,33}};
+            int[,] array2D_4 = new int[4, 4] {{00,01,02,03},{10,11,12,13},{20,21,22,23},{30,31,32,33}};
 
             int[,] array2D_6 = new int[6, 6] {{00,01,02,03,04,05},{10,11,12,13,14,15},{20,21,22,23,24,25},{30,31,32,33,34,35},{40,41,42,43,44,45},{50,51,52,53,54,55}};
-            
-            rotate90(array2D);
 
-            rotate90_CTCI(array2D);
+            Console.WriteLine("Original:");
+            PrintArray(array2D_4);
 
-            rotate90(array2D_6);
+            rotate90_CTCI(array2D_4);
+
+            rotate90(array2D_4);
+
+            Console.WriteLine("Original:");
+            PrintArray(array2D_6);
 
             rotate90_CTCI(array2D_6);
+
+            rotate90(array2D_6);
 
             Console.ReadLine();
         }
 
+        private static void rotate90(int[,] array2D)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            int temp = 0;            
+
+            int n = array2D.GetLength(0);
+
+            // add a layer for every odd number of elements. Ie: 1 element  = 1 layer
+            //                                                   2 elements = 1 layer
+            //                                                   3 elements = 2 layers
+            //                                                   4 elements = 2 layers
+            //                                                   5 elements = 3 layers etc
+            for (int layer = 0; layer < (int)Math.Ceiling( n / 2.0); ++layer)
+            {
+                // index shrinks from both sides (!) as the layer moves inward
+                for (int index = layer; index < (n - layer - 1); ++index)
+                {
+                    // Side 0
+                    // X_currentposition = index;
+                    // Y_currentposition = layer;
+                    // X_oneLayerBack = n - 1 - layer;
+                    // Y_oneLayerBack = index;
+                    temp = array2D[index, layer];
+                    array2D[index, layer] = array2D[n - 1 - layer, index];
+
+                    // Side 1
+                    // X_currentposition = n - 1 - layer;
+                    // Y_currentposition = index;
+                    // X_oneLayerBack = n - 1 - index;
+                    // Y_oneLayerBack = n - 1 - layer;                    
+                    array2D[n - 1 - layer, index] = array2D[n - 1 - index, n - 1 - layer];
+
+                    // Side 2
+                    // X_currentposition = n - 1 - index;
+                    // Y_currentposition = n - 1 - layer;
+                    // X_oneLayerBack = layer; 
+                    // Y_oneLayerBack = n - 1 - index;                    
+                    array2D[n - 1 - index, n - 1 - layer] = array2D[layer, n - 1 - index];
+
+                    // Side 3
+                    // X_currentposition = layer;
+                    // Y_currentposition = n - 1 - index;
+                    array2D[layer, n - 1 - index] = temp;
+                }
+            }        
+
+            sw.Stop();
+            
+            Console.WriteLine("Rotated (my solution): " + sw.ElapsedTicks + " ticks");
+            PrintArray(array2D);
+        }
+
+        /////////////////////////////////////////////////////////////
+        //        
+        // Below is the book's solution.
+        //
+        // Complexity: O(N^2)
+        //             The algorithm must touch all N^2 elements, so it's the best we can do
+        //
         private static void rotate90_CTCI(int[,] array2D)
         {
             Stopwatch sw = Stopwatch.StartNew();
@@ -62,10 +128,8 @@ namespace CTCI_1._6_Rotation
 
             sw.Stop();
 
-            Console.WriteLine("Rotated (CTCI):");
-            Console.WriteLine(sw.ElapsedTicks + " ticks");
+            Console.WriteLine("Rotated (book solution): " + sw.ElapsedTicks + " ticks");
             PrintArray(array2D);
-            Console.WriteLine();
         }
 
         private static void PrintArray(int[,] array2D)
@@ -83,85 +147,6 @@ namespace CTCI_1._6_Rotation
                 Console.WriteLine();
             }
 
-            Console.WriteLine();
-        }
-
-        private static void rotate90(int[,] array2D)
-        {
-            Console.WriteLine("Original:");            
-            PrintArray(array2D);
-
-            Stopwatch sw = Stopwatch.StartNew();
-
-            int temp = 0;
-            int X_currentposition = 0;
-            int Y_currentposition = 0;
-            int X_oneLayerBack = 0;
-            int Y_oneLayerBack = 0;
-
-            // add a layer for every odd number of elements. Ie: 1 element  = 1 layer
-            //                                                   2 elements = 1 layer
-            //                                                   3 elements = 2 layers
-            //                                                   4 elements = 2 layers
-            //                                                   5 elements = 3 layers etc
-            for (int layer = 0; layer < (int)Math.Ceiling( array2D.GetLength(0) / 2.0); ++layer)
-            {
-                // index shrinks from both sides (!) as the layer moves inward
-                for (int index = layer; index < (array2D.GetLength(0) - layer - 1); ++index)
-                {
-                    for (int side = 0; side < 4; ++side)
-                    {
-                        switch (side)
-                        {
-                            case 0:                            
-                                X_currentposition = index;
-                                Y_currentposition = layer;
-                                X_oneLayerBack = array2D.GetLength(0) - 1 - layer;
-                                Y_oneLayerBack = index;
-
-                                temp = array2D[X_currentposition, Y_currentposition];
-                                array2D[X_currentposition, Y_currentposition] = array2D[X_oneLayerBack, Y_oneLayerBack];
-                                break;  
-
-                            case 1:                            
-                                X_currentposition = array2D.GetLength(0) - 1 - layer;
-                                Y_currentposition = index;
-                                X_oneLayerBack = array2D.GetLength(0) - 1 - index;
-                                Y_oneLayerBack = array2D.GetLength(0) - 1 - layer;
-
-                                // write one layer backwards
-                                array2D[X_currentposition, Y_currentposition] = array2D[X_oneLayerBack, Y_oneLayerBack];
-                                break;
-
-                            case 2:                            
-                                X_currentposition = array2D.GetLength(0) - 1 - index;
-                                Y_currentposition = array2D.GetLength(0) - 1 - layer;
-                                X_oneLayerBack = layer; 
-                                Y_oneLayerBack = array2D.GetLength(0) - 1 - index;
-
-                                // write one layer backwards
-                                array2D[X_currentposition, Y_currentposition] = array2D[X_oneLayerBack, Y_oneLayerBack];
-                                break;
-
-                            case 3:
-                            default:
-                                X_currentposition = layer;
-                                Y_currentposition = array2D.GetLength(0) - 1 - index;
-
-                                // write one layer backwards
-                                array2D[X_currentposition, Y_currentposition] = temp;
-                                break;                                
-                        }
-                    }
-                }
-            }
-
-            sw.Stop();
-
-            Console.WriteLine();
-            Console.WriteLine("Rotated (Q):");
-            Console.WriteLine(sw.ElapsedTicks + " ticks");
-            PrintArray(array2D);
             Console.WriteLine();
         }
 
